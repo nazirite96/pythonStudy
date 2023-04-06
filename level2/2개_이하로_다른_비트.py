ea5086ad3f073023be228c8a48204401ad7a6237
nazirite96 <wkek805@naver.com>
2023-04-06 11:33:00 +0900

    공부
    공부

--- level2/2개_이하로_다른_비트.py
+++ level2/2개_이하로_다른_비트.py
@@ -1,15 +1,21 @@
 def solution(numbers):
     answer = []
+    print(numbers[0])
     binary_num_list = [bin(num)[2:] for num in numbers]
     binary_answer = []
     for binary_num in binary_num_list:
-        if '0' in binary_num:
-            binary_answer.append(binary_num[:binary_num.rindex('0')] + '1' + binary_num[binary_num.rindex('0')+1:])
+        print(binary_num)
+        if binary_num[-1] == '0':
+            binary_answer.append(binary_num[:-1] + '1')
         else:
-            binary_answer.append(('10'+binary_num[2:]))
+            if '0' in binary_num:
+                binary_answer.append(binary_num[:binary_num.rindex('0')] + '10' + binary_num[binary_num.rindex('0')+2:])
+            else:
+                binary_answer.append(('10'+binary_num[1:]))
 
     for bin_num in binary_answer:
+        print(bin_num)
         answer.append(int(bin_num,2))
     return answer
 
-print(solution([5,7,8,9,10,11,12]))
+print(solution([10**15]))
