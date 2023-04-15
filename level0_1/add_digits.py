dfff0c166996fd56e000876806a314c9cedb4d2d
nazirite96 <wkek805@naver.com>
2023-04-15 17:45:17 +0900

    공부
    공부

--- level0_1/add_digits.py
+++ level0_1/add_digits.py
@@ -2,7 +2,7 @@ def solution(n):
     answer = 0
     list_n = list(str(n))
     for num_s in list_n:
-        answer+= int(num_s)
+        answer += int(num_s)
     return answer
 
 print(solution(1234))
