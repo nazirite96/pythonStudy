1bdbb2c786fe69ae0f8142ee0848c1e85f9eb65d
nazirite96 <wkek805@naver.com>
2023-04-27 10:10:08 +0900

    공부
    공부

--- level2/두_큐_합_같게_만들기.py
+++ level2/두_큐_합_같게_만들기.py
@@ -1,22 +1,21 @@
 def solution(queue1, queue2):
     answer = 0
-    sum1 = sum(queue1)
-    sum2 = sum(queue2)
-    while sum1 != sum2:
-        if sum1 < sum2:
-            poped = queue2.pop(0)
-            queue1.append(poped)
-            sum1 += poped
-            sum2 -= poped
+    start = 0
+    end = len(queue1)
+    now = sum(queue1)
+    l = queue1 + queue2
+    goal = sum(l) / 2
+    while goal != now :
+        if goal > now:
+            if len(l) == end:
+                return -1
+            now += l[end]
+            end += 1
             answer += 1
-        elif sum2 < sum1:
-            poped = queue1.pop(0)
-            queue2.append(poped)
-            sum2 += poped
-            sum1 -= poped
+        else:
+            now -= l[start]
+            start += 1
             answer += 1
-        print(sum1, sum2)
-
 
     return answer
 
@@ -34,4 +33,10 @@ print(solution(queue1, queue2))
 queue1 = [1, 1]
 queue2 = [1, 5]
 
-# print(solution(queue1, queue2))
+print(solution(queue1, queue2))
+
+
+queue1 = [1,2,9,4]
+queue2 = [1,1,2,2]
+
+print(solution(queue1, queue2))
