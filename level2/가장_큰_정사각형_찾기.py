0b532b4d6ddbb19e93ecb94496d19288bf8d5202
nazirite96 <wkek805@naver.com>
2023-05-24 10:10:03 +0900

    공부
    공부

--- level2/가장_큰_정사각형_찾기.py
+++ level2/가장_큰_정사각형_찾기.py
@@ -1,30 +1,3 @@
-# import math
-# def check_square(x,y,n,board):
-#     for i in range(0, n ):
-#         if 0 in board[y+i][x:x+n]:
-#             return False
-#     return True
-#
-# def solution(board):
-#     answer = 0
-#
-#     count_one = sum([line.count(1) for line in board])
-#     print(count_one)
-#     max_n = int(math.sqrt(count_one))
-#     check_flag = False
-#     for n in range(max_n,0,-1):
-#         for y in range(0, len(board)-n+1):
-#             for x in range(0,len(board[0])-n+1):
-#                 check_flag = square = check_square(x, y, n, board)
-#                 if check_flag:
-#                     break
-#             if check_flag:
-#                 break
-#         if check_flag:
-#             answer = n * n
-#             break
-#     return answer
-
 def solution(board):
     width = len(board[0])
     height = len(board)
@@ -38,9 +11,3 @@ def solution(board):
 
 print(solution([[0,0,1,1],[1,1,1,1]]))
 print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
-
-
-[[0, 1, 1, 1],
- [1, 1, 2, 2],
- [1, 2, 2, 3],
- [0, 0, 1, 0]]
--- level2/숫자_카드_나누기.py
+++ level2/숫자_카드_나누기.py
@@ -3,26 +3,35 @@ import math
 
 def solution(arrayA, arrayB):
     answer = 0
-    gcd = math.gcd(arrayA)
-    a_yacso_list = [i for i in range(3,min(arrayA)+1,2)
-                    if arrayA[0] % i == 0 and arrayA[1] % i == 0 ]
-    b_yacso_list = [i for i in range(3,min(arrayB)+1,2)
-                    if arrayB[0] % i == 0 and arrayB[1] % i == 0 ]
-    if arrayA[0] % 2 == 0 and arrayA[1] % 2 == 0:
-        a_yacso_list.append(2)
-    if arrayB[0] % 2 == 0 and arrayB[1] % 2 == 0:
-        b_yacso_list.append(2)
-    for a_yacsoo in a_yacso_list:
-        if arrayB[0] % a_yacsoo !=0 and arrayB[1] % a_yacsoo != 0:
-            if answer < a_yacsoo:
-                answer = a_yacsoo
-    for b_yacsoo in b_yacso_list:
-        if arrayA[0] % b_yacsoo !=0 and arrayA[1] % b_yacsoo != 0:
-            if answer < b_yacsoo:
-                answer = b_yacsoo
+    gcd_a = arrayA[0]
+    gcd_b = arrayB[0]
+    for a in arrayA:
+        gcd_a = math.gcd(gcd_a,a)
+    for b in arrayB:
+        gcd_b = math.gcd(gcd_b,b)
+    cant_div_a_to_b = True
+    cant_div_b_to_a = True
+    for a in arrayA:
+        if a % gcd_b == 0 or gcd_b == 1:
+            cant_div_a_to_b = False
+            break
+    for b in arrayB:
+        if b % gcd_a == 0 or gcd_a == 1:
+            cant_div_b_to_a = False
+            break
+    if cant_div_b_to_a and cant_div_a_to_b:
+        return max(gcd_b,gcd_a)
+    elif cant_div_a_to_b:
+        return gcd_b
+    elif cant_div_b_to_a:
+        return gcd_a
+
     return answer
 
 a = [100000000,100000000]
+b = [5, 20]
+
+a = [10, 17]
 b = [5, 20]
 
 print(solution(a,b))
@@ -34,4 +43,16 @@ print(solution(a,b))
 
 a = [14, 35, 119]
 b = [18, 30, 102]
+print(solution(a,b))
+
+a = [14, 35, 119]
+b = [18, 30, 102]
+print(solution(a,b))
+
+a = [5]
+b = [8]
+print(solution(a,b))
+
+a = [10, 20]
+b = [5, 17, 20]
 print(solution(a,b))
