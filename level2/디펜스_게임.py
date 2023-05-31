883854cd4e13f40ae0076bdb723631731eaaee3a
nazirite96 <wkek805@naver.com>
2023-05-31 16:58:43 +0900

    공부
    공부

--- level2/디펜스_게임.py
+++ level2/디펜스_게임.py
@@ -1,7 +1,31 @@
+import heapq
+
 def solution(n, k, enemy):
     answer = 0
+    heapq_list = []
+    for i, e in enumerate(enemy):
+        n -= e
+        heapq.heappush(heapq_list,-e)
+        if n >= 0:
+            answer += 1
+        else:
+            while k > 0 > n:
+                used_k = heapq.heappop(heapq_list)
+                n -= used_k
+                k -= 1
+            if n >= 0:
+                answer += 1
+            else:
+                break
     return answer
 
 n = 7
 k = 3
 enemy = [4, 2, 4, 5, 3, 3, 1]
+
+print(solution(n, k, enemy))
+
+n = 2
+k = 4
+enemy = [3, 3, 3, 3]
+print(solution(n, k, enemy))
