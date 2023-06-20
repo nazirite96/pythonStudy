4759da79e8246206c1d92e0133976182db1c455c
nazirite96 <wkek805@naver.com>
2023-06-20 15:32:55 +0900

    공부
    공부

--- level2/n_queen.py
+++ level2/n_queen.py
@@ -1,12 +1,25 @@
+answer = 0
 def solution(n):
-    answer = 0
-    for y in range(n):
-        for x in range(n):
-            check_list = [[True for _ in range(n)] for _ in range(n)]
-            answer += case_queen(x,y,check_list,0)
+    global answer
+    row = [True]*n
+    r_dig = [True]*2*n
+    l_dig = [True]*2*n
+
+    def queen(r, remain):
+        global answer
+        if remain:
+            for idx,i in enumerate(remain):
+                if row[i] and r_dig[r-i] and l_dig[r+i]:
+                    next_remain = remain[:idx] + remain[idx+1:]
+                    row[i] = r_dig[r-i] = l_dig[r+i] = False
+                    queen(r+1, next_remain)
+                    row[i] = r_dig[r-i] = l_dig[r+i] = True
+        else:
+            answer += 1
+
+    queen(0, list(range(n)))
+
     return answer
 
-def case_queen(x,y,check_list,count):
-    pass
 
-print(solution(4))
+print(solution(5))
