168f03a4a810c57b3973c6655681471a7cec8ca5
nazirite96 <wkek805@naver.com>
2023-09-04 14:51:12 +0900

    공부
    공부

--- level2/n_queen.py
+++ level2/n_queen.py
@@ -1,13 +1,16 @@
 answer = 0
+
+
 def solution(n):
     global answer
     row = [True]*n
-    r_dig = [True]*2*n
-    l_dig = [True]*2*n
+    r_dig = [True]*(2*n)
+    l_dig = [True]*(2*n)
+
     def queen(r, remain):
         global answer
         if remain:
-            for idx,i in enumerate(remain):
+            for idx, i in enumerate(remain):
                 if row[i] and r_dig[r-i] and l_dig[r+i]:
                     next_remain = remain[:idx] + remain[idx+1:]
                     row[i] = r_dig[r-i] = l_dig[r+i] = False
@@ -19,4 +22,6 @@ def solution(n):
     queen(0, list(range(n)))
 
     return answer
+
+
 print(solution(4))
