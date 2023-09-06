03c940391b57ab1411383b803004180e6487669e
nazirite96 <wkek805@naver.com>
2023-09-06 16:13:51 +0900

    공부
    공부

--- level2/두_원_사이의_정수_쌍.py
+++ level2/두_원_사이의_정수_쌍.py
@@ -1,3 +1,16 @@
+import math
+
+
 def solution(r1, r2):
     answer = 0
-    return answer
+
+    for y in range(0,r2+1):
+        for x in range(1,r2+1):
+            a = math.sqrt(((x*x) + (y*y)))
+            if r2 >= a >= r1:
+                answer += 1
+
+    return answer * 4
+
+
+print(solution(4,6))
